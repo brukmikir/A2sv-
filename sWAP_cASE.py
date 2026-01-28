83df300028fde7e70790099daa8cdc45191f009c
unknown <brukmikir>
2026-01-28 11:33:56 -0800

    third

---
+++ sWAP_cASE.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    
+    res=[]
+    
+    for i in s:
+        if i.isupper():
+            res += i.lower()
+        elif i.islower():
+            res += i.upper()
+        else:
+            res += i
+            
+    return ''.join(res)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
