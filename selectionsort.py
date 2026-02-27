863e4afa9f4dba00ebd5c260e828f2b0e9f26605
unknown <brukmikir>
2026-02-27 06:41:50 -0800

    selection

---
+++ selectionsort.py
@@ -0,0 +1,14 @@
+class Solution:
+    def selectionSort(self, arr):
+        n = len(arr)
+        
+        for i in range(n):
+            smallest = i
+            
+            for j in range(i + 1, n):
+                if arr[j] < arr[smallest]:
+                    smallest = j
+            
+            arr[i], arr[smallest] = arr[smallest], arr[i]
+        
+        return arr
